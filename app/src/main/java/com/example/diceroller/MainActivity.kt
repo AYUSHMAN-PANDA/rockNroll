fa1c4f4b47424bc6f677395788948e4bb07edd72
Aayush <panda.ayushman@gmail.com>
2021-06-13 13:26:34 +0530

    dice roller with button and TextView

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled !", Toast.LENGTH_SHORT)
+            toast.show()
+
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val dice=Dice(6)
+        val result = dice.roll()
+
+        val resultText : TextView = findViewById(R.id.textView)
+        resultText.text = result.toString()
+    }
+
+    class Dice(private val no_sides: Int){
+        fun roll(): Int{
+             return (1..no_sides).random()
+        }
     }
 }
